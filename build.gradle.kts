a80e57497e870d11316dad0539ef8c142cc1fbab
Bruno Essmann <bruno.essmann@gmail.com>
2022-07-11 12:36:18 +0200

    Adjusted Java toolchain language version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
     withSourcesJar()
 }
